b3bc4cbd581580d77fb53c58f15d9341b04e790c
kangyeon9525 <kangyeon9525@gmail.com>
2024-05-17 16:52:26 +0900

    회원가입/회원탈퇴/로그인 REST API, jwt token, MySQL, Swagger 설정, API 요청 후의 응답을 application/json형태로 변환, github action

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,9 @@ APP_NAME=dopamineKiller
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
+echo "> 현재 실행 중인 애플리케이션 PID 확인"
 CURRENT_PID=$(pgrep -f $APP_NAME)
+echo "CURRENT_PID: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
   echo "> 종료할 애플리케이션이 없습니다."
@@ -19,3 +21,5 @@ fi
 
 echo "> Deploy - $JAR_PATH"
 nohup java -jar $JAR_PATH > /home/ubuntu/atm/deploy.log 2>&1 &
+echo "java -jar $JAR_PATH 명령 실행 완료"
+
