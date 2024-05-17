d3130e5587c73a73f1e83b143f8cd28123c31a4c
kangyeon9525 <kangyeon9525@gmail.com>
2024-05-17 16:21:47 +0900

    회원가입/회원탈퇴/로그인 REST API, jwt token, MySQL, Swagger 설정, API 요청 후의 응답을 application/json형태로 변환, github action

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,14 +9,13 @@ JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z $CURRENT_PID ]
-then
+if [ -z $CURRENT_PID ]; then
   echo "> 종료할 애플리케이션이 없습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
+  echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> Deploy - $JAR_PATH "
+echo "> Deploy - $JAR_PATH"
 nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
